9cc1ab49f9e8b914b8c41f29afcef9480c332424
Grant Greer <grant@MacBook-Pro-3.local>
2022-01-21 06:58:05 -0800

    Added createElement method

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// The element declaration below is equivalent to the result of calling
+/*// The element declaration below is equivalent to the result of calling
 // const element = React.createElement("h1", { title: "foo" }, "Hello");
 // An element is essentially an object with two properties: type and props
 const element = {
@@ -10,5 +10,60 @@ const element = {
     children: "Hello",
   },
 };
+
 const container = document.getElementById("root");
-ReactDOM.render(element, container);
+// Render is the method that changes the DOM
+// ReactDOM.render(element, container), Below is a render equivalent.
+// First we create a Node from the react element
+// afterwards, we assign all of the element props to that node
+const node = document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+// textNode is appended to the H1 and the H1 to the container
+node.appendChild(text);
+container.appendChild(node); */
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+function render(element, container) {
+  // TODO create dom nodes
+}
+
+const Didact = {
+  createElement,
+  render,
+};
+
+/** @jsx Didact.createElement */
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
+
+const container = document.getElementById("root");
+Didact.render(element, container);
