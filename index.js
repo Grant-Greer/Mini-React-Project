7eed4955af8cac8f8f7751ba497f7552c8011c5e
Grant Greer <grant@MacBook-Pro-3.local>
2022-01-21 09:21:16 -0800

    Created Render Method

--- index.js
+++ index.js
@@ -49,7 +49,42 @@ function createTextElement(text) {
 }
 
 function render(element, container) {
-  // TODO create dom nodes
+    const dom =
+      element.type == "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(element.type)
+  ​
+    const isProperty = key => key !== "children"
+    Object.keys(element.props)
+      .filter(isProperty)
+      .forEach(name => {
+        dom[name] = element.props[name]
+      })
+  ​
+    element.props.children.forEach(child =>
+      render(child, dom)
+    )
+  ​
+    container.appendChild(dom)
+  }
+
+  let nextUnitOfWork = null
+​
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork
+    )
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+​
+requestIdleCallback(workLoop)
+​
+function performUnitOfWork(nextUnitOfWork) {
+  // TODO
 }
 
 const Didact = {
